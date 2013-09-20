add3a1ece6ade99b5eef7c113fd2f6cc43454a35
Cyril Robin <cyril.robin@laas.fr>
2013-09-20 22:48:44 +0200

    [codel] Automatically generated Ctrl codel

---
+++ codels/xaresCntrlTaskCodels.cc
@@ -0,0 +1,16 @@
+/**
+ ** xaresCntrlTaskCodels.cc
+ **
+ ** Codels used by the control task xaresCntrlTask
+ **
+ ** Author: 
+ ** Date: 
+ **
+ **/
+
+#include <portLib.h>
+
+#include "server/xaresHeader.h"
+
+
+
